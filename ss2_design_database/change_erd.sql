a827ad630079ffc82fe81c383c311cce564380c6
unknown <you@example.com>
2023-05-15 14:02:13 +0700

    em nop bai tap ngay 15/05 a

--- ss2_design_database/change_erd.sql
+++ ss2_design_database/change_erd.sql
@@ -1,30 +1,30 @@
 create database changeERD;
 use changeERD;
 
-create table NHACC (
+create table nha_cc (
 MaNCC int primary key auto_increment,
 TenNCC varchar(50),
 DiaChi varchar(255)
 );
 
-create table DONDH(
+create table don_dh(
 SoDH int primary key auto_increment,
 NgayDH date,
 MaNCC int,
-foreign key (MaNCC) references NHACC(MaNCC)
+foreign key (MaNCC) references nha_cc(MaNCC)
 );
 
-create table PHIEUNHAP(
+create table phieu_nhap(
 SoPN int primary key auto_increment,
 NgayNhap date
 );
 
-create table PHIEUXUAT(
+create table phieu_xuat(
 SoPX int primary key auto_increment,
 NgayXuat date
 );
 
-create table VATTU(
+create table vat_tu(
 MAVTU int primary key auto_increment,
 TenVTU varchar(50)
 );
@@ -35,8 +35,8 @@ SLXuat int,
 SoPX int,
 MaVTU int,
 primary key(SoPX,MaVTU),
-foreign key (SoPX) references PHIEUXUAT(SoPX),
-foreign key (MaVTU) references VATTU(MaVTU)
+foreign key (SoPX) references phieu_xuat(SoPX),
+foreign key (MaVTU) references vat_tu(MaVTU)
 );
 
 create table chi_tiet_PN(
@@ -45,20 +45,20 @@ SLNhap int,
 SoPN int,
 MaVTU int,
 primary key(MaVTU,SoPN),
-foreign key (MaVTU) references VATTU(MaVTU),
-foreign key (SoPN) references PHIEUNHAP(SoPN)
+foreign key (MaVTU) references vat_tu(MaVTU),
+foreign key (SoPN) references phieu_nhap(SoPN)
 );
 
 create table chi_tiet_DDH(
 SoDH int,
 MaVTU int,
 primary key(SoDH,MaVTU),
-foreign key (MaVTU) references VATTU(MaVTU),
-foreign key (SoDH) references DONDH(SoDH)
+foreign key (MaVTU) references vat_tu(MaVTU),
+foreign key (SoDH) references don_dh(SoDH)
 );
 
-create table SDT(
+create table sdt(
 MaNCC int,
 SDT varchar(50) primary key,
-foreign key (MaNCC) references NHACC(MaNCC)
+foreign key (MaNCC) references nha_cc(MaNCC)
 )
