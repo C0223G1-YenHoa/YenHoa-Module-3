abecf3a057e79d197b3fe9b66fd56add2a72dac3
unknown <you@example.com>
2023-05-16 15:44:35 +0700

    em nop bai tap ngay 16/05 a

---
+++ ss3_operation_database/quan_ly_ban_hang_ss3.sql
@@ -0,0 +1,77 @@
+create database quan_ly_ban_hang;
+use quan_ly_ban_hang;
+
+create table customer(
+c_id int primary key auto_increment,
+c_name varchar(25),
+c_age int
+);
+
+create table ordered(
+o_id int primary key auto_increment,
+c_id int,
+o_date date,
+o_total_price double,
+foreign key (c_id) references customer(c_id)
+);
+
+create table product(
+p_id int primary key auto_increment,
+p_name varchar(50),
+p_price double
+);
+
+create table order_detail(
+o_id int,
+p_id int,
+od_qty int ,
+primary key (o_id,p_id),
+foreign key (o_id) references ordered(o_id),
+foreign key (p_id) references product(p_id)
+);
+
+insert into customer(c_name,c_age)
+value ("Minh Quan",10),
+("Ngoc Oanh",20),
+("Hong Ha",50);
+
+insert into ordered(c_id,o_date,o_total_price )
+value (1,20060321,Null),
+(2,20060323,Null),
+(1,20060316,Null);
+
+insert into product(p_name,p_price)
+value ("May Giat",3),
+("Tu lanh",5),
+("Dieu hoa",7),
+("Quat",1),
+("Bep dien",2);
+
+insert into order_detail(o_id,p_id,od_qty)
+value (1,1,3),
+(1,3,7),
+(1,4,2),
+(2,1,1),
+(3,1,8),
+(2,5,4),
+(2,3,3);
+
+select o_id,o_date,o_total_price
+from ordered;
+
+select customer.c_name,product.p_name,o_date
+from customer
+join ordered on customer.c_id=ordered.c_id
+join order_detail on ordered.o_id=order_detail.o_id
+join product on order_detail.p_id=product.p_id;
+
+select customer.c_name
+from customer
+left join ordered on customer.c_id=ordered.c_id
+where ordered.o_id is null;
+
+select ordered.o_id,ordered.o_date, sum(order_detail.od_qty * product.p_price) total
+from ordered
+join order_detail on ordered.o_id=order_detail.o_id
+join product on order_detail.p_id=product.p_id
+group by ordered.o_id;
---
+++ ss3_operation_database/quan_ly_sinh_vien.sql
@@ -0,0 +1,87 @@
+create database quan_ly_sinh_vien;
+use quan_ly_sinh_vien;
+
+create table class(
+class_id int primary key not null auto_increment,
+class_name varchar(60) not null,
+start_date datetime not null,
+status bit
+);
+
+create table student(
+student_id int primary key auto_increment not null,
+student_name varchar(30) not null,
+address varchar(50),
+phone varchar(20),
+status bit,
+class_id int not null,
+foreign key (class_id) references class(class_id)
+);
+
+create table subjects(
+sub_id int primary key not null auto_increment,
+sub_name varchar(30) not null,
+credit tinyint not null default 1 check (credit>=1),
+status bit default 1
+);
+
+create table Mark (
+mark_id int primary key auto_increment not null,
+sub_id int not null,
+student_id int not null,
+phone varchar(20),
+mark float default 0 check (mark between 0 and 100),
+exam_times tinyint default 1,
+unique (sub_id,student_id),
+foreign key (sub_id) references subjects(sub_id),
+foreign key (student_id) references student(student_id)
+);
+
+INSERT INTO class
+VALUES (1, 'A1', '2008-12-20', 1);
+INSERT INTO class
+VALUES (2, 'A2', '2008-12-22', 1);
+INSERT INTO class
+VALUES (3, 'B3', current_date, 0);
+
+INSERT INTO student (student_name, address, phone, status, class_id)
+VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
+INSERT INTO student (student_name, address, status, class_id)
+VALUES ('Hoa', 'Hai phong', 1, 1);
+INSERT INTO student (student_name, address, phone, status, class_id)
+VALUES ('Manh', 'HCM', '0123123123', 0, 2);
+
+INSERT INTO subjects
+VALUES (1, 'CF', 5, 1),
+       (2, 'C', 6, 1),
+       (3, 'HDJ', 5, 1),
+       (4, 'RDBMS', 10, 1);
+       
+INSERT INTO mark (sub_id, student_id, mark, exam_times)
+VALUES (1, 1, 8, 1),
+       (1, 2, 10, 2),
+       (2, 1, 12, 1);
+       
+       select *
+       from student
+       where substring_index(student.student_name," ",-1) like "H%";
+       
+       select *
+       from class
+       where month(start_date)=12;
+       
+       select*
+       from subjects
+       where credit between 3 and 5;
+       
+       set sql_safe_updates=0;
+       update student set class_id = 2 
+       where substring_index(student.student_name," ",-1)="hung";
+       set sql_safe_updates=1;
+       
+       select student.student_name,subjects.sub_name,mark
+       from mark 
+       join student on mark.student_id=student.student_id
+       join subjects on subjects.sub_id=subjects.sub_id
+       order by mark.mark desc;
+       
