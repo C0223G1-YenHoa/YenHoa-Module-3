9b172c1a445dd6b024652a32e26f76f2a41755a4
OS <lysh31@gmail.com>
2023-06-05 16:35:03 +0700

    em nop bai tap ngay 05/06

--- ss12_jdbc_crud/user.sql
+++ ss12_jdbc_crud/user.sql
@@ -14,3 +14,34 @@ insert into users(name, email, country) values('Hoa','hoa@codegym.vn','Viet Nam'
 insert into users(name, email, country) values('Haan','han@codegym.vn','Viet Nam');
 insert into users(name, email, country) values('Kante','kante@che.uk','Kenia');
 
+delimiter //
+create procedure display_all()
+begin 
+select * from users;
+end //
+delimiter ;
+
+call display_all();
+
+
+delimiter //
+create procedure update_user(new_id int,new_name varchar(50),email varchar(50),country varchar(50))
+begin 
+set sql_safe_updates=0;
+update users set name = new_name , email= email, country = country where id = new_id;
+set sql_safe_updates=1;
+end //
+delimiter ;
+
+call update_user(6,"hoa","hoa@gmail.com","VN");
+
+delimiter //
+create procedure delete_user(id_delete int)
+begin 
+set sql_safe_updates=0;
+delete from users where id = id_delete;
+set sql_safe_updates=1;
+end //
+delimiter ;
+
+call delete_user(5);
